81cfc6752180f8b8d46efd12ce4b8762fd42e0db
Dmitriy <nevermind1979@mail.ru>
2021-07-25 09:14:23 +0300

    add Thread

---
+++ Thread/Main.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
+    public static void main(String[] args) {
+     foo1();
+     foo2();
+        }
+
+    static void foo1 () {
+        float[] arr = new float[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis() - a);
+    }
+    static void foo2() {
+        float[] arr = new float[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        thread1.start();
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        thread2.start();
+        System.arraycopy(arr1,0,arr,0,h);
+        System.arraycopy(arr2,0,arr,h,h);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+}
