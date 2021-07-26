9da9e2dec2c145912b6428e2b30fd41c70c8dee4
Дмитрий Олегович Петров <77935565+0202-D@users.noreply.github.com>
2021-07-26 18:45:42 +0300

    Update Main.java

--- Thread/Main.java
+++ Thread/Main.java
@@ -34,15 +34,19 @@ public class Main {
             for (int i = 0; i < arr1.length; i++) {
                 arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-        });
-        thread1.start();
+        }); 
         Thread thread2 = new Thread(() -> {
             for (int i = 0; i < arr2.length; i++) {
                 arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
-
+        thread1.start();
         thread2.start();
+          try {
+            thread1.join();
+            thread2.join();
+        }
+        catch (InterruptedException e) { e.printStackTrace();}
         System.arraycopy(arr1,0,arr,0,h);
         System.arraycopy(arr2,0,arr,h,h);
         System.out.println(System.currentTimeMillis() - a);
